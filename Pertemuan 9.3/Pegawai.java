c5ba24a7012fa3df6bcc5d964bdaf52b43b9e687
Arif Rizky <162144873+JajaTRPL@users.noreply.github.com>
2024-05-21 18:46:08 +0700

    Add files via upload

---
+++ Pertemuan 9.3/Pegawai.java
@@ -0,0 +1,75 @@
+public class Pegawai {
+    private String nama;
+    private String departemen;
+    private char gender;
+    private double gaji;
+    private int cuti;
+    protected String nip;
+
+    public Pegawai(String nama, String departemen, char gender) {
+        this.nama = nama;
+        this.departemen = departemen;
+        this.gender = gender;
+    }
+
+    public void setGaji(double gaji) {
+        this.gaji = gaji;
+    }
+
+    public double getGaji() {
+        return gaji;
+    }
+
+    public void setCuti(int cuti) {
+        this.cuti = cuti;
+    }
+
+    public void setCuti(String alasan) {
+        if (alasan.equalsIgnoreCase("Pernikahan")) {
+            this.cuti = 12;
+        } else if (alasan.equalsIgnoreCase("persalinan")) {
+            this.cuti = 20;
+        } else {
+            this.cuti = 10; 
+        }
+    }
+
+    public int getCuti() {
+        return cuti;
+    }
+
+    public void setNip(String nip) {
+        if (nip.startsWith("P")) {
+            this.nip = nip;
+        } else {
+            System.out.println("NIP tidak valid untuk Pegawai. Harus diawali dengan 'P'.");
+        }
+    }
+
+    public void setNip(int nip) {
+        this.nip = String.format("P%04d", nip);
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getDepartemen() {
+        return departemen;
+    }
+
+    public void displayinfo() {
+        System.out.println("Jabatan : " + this.getClass().getSimpleName());
+        System.out.println("Nama : " + getNama());
+        System.out.println("Departemen : " + getDepartemen());
+        System.out.println("NIP : " + getNip());
+        System.out.println("Gaji : " + getGaji());
+        System.out.println("Gaji : " + getGaji() * 2);
+        System.out.println("Cuti : " + (getCuti() + 12));
+        System.out.println("===================================");
+    }
+}
